04c8c52a6219596a66cdb2c339f6e955f063e07e
Radyko <122955109+Radyko@users.noreply.github.com>
2025-01-02 08:54:10 -0600

    Working UI without order matching

--- my-app/src/main/java/com/mycompany/app/MainApp.java
+++ my-app/src/main/java/com/mycompany/app/MainApp.java
@@ -3,9 +3,7 @@ package com.mycompany.app;
 public class MainApp {
     public static void main(String[] args) {
         OrderDatabaseManager databaseManager = new OrderDatabaseManager();
-
         Runtime.getRuntime().addShutdownHook(new Thread(databaseManager::closeConnections));
-
         new OrderUI(databaseManager);
     }
 }
